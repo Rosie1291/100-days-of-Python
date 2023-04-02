d284315542ceaf335d4db56b99bcfacce046b060
nhung1291 <hongnhungnguyen1291@gmail.com>
2023-04-02 00:32:17 -0400

    fix: code logic

--- day-9/blind-auction/main.py
+++ day-9/blind-auction/main.py
@@ -1,7 +1,6 @@
 import os
 import art
 
-
 # function to clear the console
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -13,19 +12,7 @@ print("Welcome to the secret auction program!")
 
 # Initiate dictionary for users' input
 bidders = {}
-
-# Program
-while True:
-    user = input("What is your name?: ")
-    bid = int(input("What's your bid?: $"))
-    bidders[user] = bid
-    next = input("Are there any other bidders? Type 'yes' or 'no'.\n")
-    if next == "yes":
-        # call the clear function
-        clear()
-        continue
-    else:
-        break
+bidding_done = False
 
 def findWinner(bidders):
     maxBid = 0
@@ -35,6 +22,17 @@ def findWinner(bidders):
             winner = user
     print("The winner is " + winner + " with a bid of $" + str(maxBid) + ".")
 
-findWinner(bidders)
+while not bidding_done:
+    user = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bidders[user] = bid
+    next = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if next == "no":
+        bidding_done = "True"
+        findWinner(bidders)
+    elif next == "yes":
+        # call the clear function
+        clear()
+        continue
                
 
